706d105ab932ec8c3a391eb3f9a42d5c29a16195
FarahBushra <farah.bushra@northsouth.edu>
2022-12-20 18:06:38 +0600

    updated backend files

---
+++ Backend/updated files/laravel-sanctum-api/database/migrations/2022_12_20_083646_create_account_heads_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAccountHeadsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('account_heads', function (Blueprint $table) {
+            $table->id();
+            $table->string('head_code')->index();
+            $table->string('title');
+            $table->string('type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('account_heads');
+    }
+}
---
+++ Backend/updated files/laravel-sanctum-api/database/migrations/2022_12_20_101134_create_transactions_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+            $table->string('acc_head');
+            $table->foreign('acc_head')->references('head_code')->on('account_heads');
+
+            $table->string('voucher_no');
+            $table->string('voucher_id');
+            $table->longText('narration')->nullable();
+            $table->dateTime('transaction_at');
+            $table->decimal('debit', 10, 2)->nullable();
+            $table->decimal('credit', 10, 2)->nullable();
+            $table->boolean('is_opening');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
